8093e807a6b97509602108f2da4d62c339708d56
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-04-07 12:54:14 +0300

    2 done

--- js/task-02.js
+++ js/task-02.js
@@ -14,3 +14,15 @@ const ingredients = [
 
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
 // после чего вставит все li за одну операцию в список ul.ingredients.Для создания DOM - узлов используй document.createElement().
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = option;
+
+  return itemEl;
+});
+console.log(elements);
+
+ingredientsEl.append(...elements);
